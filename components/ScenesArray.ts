27dbcddbcd63265f10defbcad11558231ea408a5
Bernard De Guzman <deguzmanbernardandrew@gmail.com>
2024-09-24 02:22:07 +0800

    feat: add tour component and poc scenes

---
+++ components/ScenesArray.ts
@@ -0,0 +1,170 @@
+const scenesArray = {
+    lobby: {
+        sceneName: 'lobby',
+        slug: 'lobby',
+        scenePanoImg: './images/lobby.jpeg',
+        initPitch: 0.9281912889787105,
+        initYaw: 126.87471048680484,
+        hotSpotsArr: [
+            {
+                pitch: -1.2139652395569707,
+                yaw: 170.02892314325766,
+                transition: 'centre',
+                type: 'custom',
+            },
+            {
+                pitch: -1.6878199836975938,
+                yaw: 97.03543091518348,
+                transition: 'studiob',
+                type: 'custom',
+            },
+            {
+                pitch: 0.018403293588653363,
+                yaw: 106.37870184669043,
+                transition: 'studioc',
+                type: 'custom',
+            },
+            {
+                pitch: 1.5111661556071483,
+                yaw: 116.08738619720158,
+                transition: 'kitchen',
+                type: 'custom',
+            },
+        ],
+    },
+    centre: {
+        sceneName: 'studioAcentre',
+        slug: 'centre',
+        scenePanoImg: './images/studio-A_centre.jpeg',
+        initPitch: -2.7342254361971903,
+        initYaw: -71.59834061057227,
+        hotSpotsArr: [
+            {
+                pitch: 0.15370731784588995,
+                yaw: -150.75566169430155,
+                transition: 'lobby',
+                type: 'custom',
+            },
+            {
+                pitch: -3.079329854551136,
+                yaw: -75.95322066806996,
+                transition: 'control',
+                type: 'custom',
+            },
+            {
+                pitch: -5.563298144923796,
+                yaw: 153.60346485120957,
+                transition: 'amps',
+                type: 'custom',
+            },
+            {
+                pitch: -15.580099767468806,
+                yaw: -57.42241290441442,
+                type: 'info',
+                text: 'Sonor Signature Series',
+                url: '',
+            },
+        ],
+    },
+
+    control: {
+        sceneName: 'studioAcontrol',
+        slug: 'control',
+        scenePanoImg: './images/studio-A_control.jpeg',
+        initPitch: -19.039397382494883,
+        initYaw: -6.074582550811249,
+        hotSpotsArr: [
+            {
+                pitch: -0.6823584269296668,
+                yaw: -48.057332810223606,
+                transition: 'centre',
+                type: 'custom',
+            },
+            {
+                pitch: 0.8344510119563971,
+                yaw: 26.559165217764185,
+                transition: 'lobby',
+                type: 'custom',
+            },
+            {
+                pitch: -39.63498815234347,
+                yaw: 45.5933503369788,
+                type: 'info',
+                text: 'Mac Pro',
+            },
+        ],
+    },
+    amps: {
+        sceneName: 'studioAamps',
+        slug: 'amps',
+        scenePanoImg: './images/studio-A_amps.jpeg',
+        initPitch: -14.67870461927905,
+        initYaw: -4.15014513356831,
+        hotSpotsArr: [
+            {
+                pitch: 5.415471053908403,
+                yaw: 107.39929697097236,
+                transition: 'lobby',
+                type: 'custom',
+            },
+            {
+                pitch: -2.5086127697264193,
+                yaw: -118.71730610610837,
+                transition: 'centre',
+                type: 'custom',
+            },
+        ],
+    },
+    studiob: {
+        sceneName: 'studioB',
+        slug: 'studiob',
+        scenePanoImg: './images/studio-B.jpeg',
+        initPitch: -15.786736656954153,
+        initYaw: -39.455927353368175,
+        hotSpotsArr: [
+            {
+                pitch: -5.769933454049382,
+                yaw: -139.9868101390428,
+                transition: 'lobby',
+                type: 'custom',
+            },
+        ],
+    },
+    studioc: {
+        sceneName: 'studioC',
+        slug: 'studioc',
+        scenePanoImg: './images/studio-C.jpeg',
+        initPitch: -17.401790016227125,
+        initYaw: 95.4232590874534,
+        hotSpotsArr: [
+            {
+                pitch: -5.507104189474294,
+                yaw: 98.9641219316082,
+                transition: 'lobby',
+                type: 'custom',
+            },
+        ],
+    },
+    kitchen: {
+        sceneName: 'kitchen',
+        slug: 'kitchen',
+        scenePanoImg: './images/kitchen.jpeg',
+        initPitch: -7.403534011791035,
+        initYaw: -82.7456174211854,
+        hotSpotsArr: [
+            {
+                pitch: -3.5175384513404873,
+                yaw: 109.43983094512774,
+                transition: 'lobby',
+                type: 'custom',
+            },
+            {
+                pitch: -2.098026320919447,
+                yaw: 122.7445434586991,
+                transition: 'centre',
+                type: 'custom',
+            },
+        ],
+    },
+}
+export default scenesArray
---
+++ components/Tour.tsx
@@ -0,0 +1,133 @@
+'use client'
+
+import { Pannellum } from 'pannellum-react'
+import * as React from 'react'
+import scenesArray from './ScenesArray'
+
+/**
+ * Tour component isn't currently supported
+ * in the pannellum-react library: https://www.npmjs.com/package/pannellum-react
+ *
+ * * Original Unwrapped Pannellum Tour Component:
+ * * https://pannellum.org/documentation/examples/tour/
+ *
+ * Thankfully, someone created a component for it 🙏🙏🙏
+ * The Images and the Data are from the codesandbox
+ * * https://codesandbox.io/p/sandbox/pannellum-react-tour-mnopwk?file=%2Fsrc%2Fpartials%2FScenesArray.js
+ *
+ * ? TODO: add type declarations (on actual project, not on this POC)
+ * TODO: Fix initial hotspots
+ */
+function Tour() {
+    const [currentScene, setCurrentScene] = React.useState('lobby')
+    const [yaw, setYaw] = React.useState(0)
+    const [pitch, setPitch] = React.useState(0)
+    const [hfov, setHfov] = React.useState(100)
+    const panImage = React.useRef(null)
+
+    const hotspotIcon = (hotSpotDiv) => {
+        const image = document.createElement('img')
+        image.classList.add('hotspoticon')
+        image.setAttribute('width', '30')
+        image.setAttribute('height', '30')
+        image.setAttribute('src', './images/circle-chevron-up-solid.svg')
+        hotSpotDiv.appendChild(image)
+    }
+
+    const infoIcon = (hotSpotDiv) => {
+        const image = document.createElement('img')
+        image.classList.add('infoicon')
+        image.setAttribute('width', '30')
+        image.setAttribute('height', '30')
+        image.setAttribute(
+            'src',
+            'https://img.icons8.com/material/4ac144/256/info.png'
+        )
+        hotSpotDiv.appendChild(image)
+    }
+
+    const hotSpotIcon = (type) => {
+        if (type === 'custom') {
+            return hotspotIcon
+        } else if (type === 'info') {
+            return infoIcon
+        }
+    }
+
+    const moveForward = () => {
+        setPitch((prevPitch) => prevPitch + 10) // Adjust the value for speed
+    }
+
+    const moveBackward = () => {
+        setPitch((prevPitch) => prevPitch - 10) // Adjust the value for speed
+    }
+
+    const moveLeft = () => {
+        setYaw((prevYaw) => prevYaw - 10) // Adjust the value for speed
+    }
+
+    const moveRight = () => {
+        setYaw((prevYaw) => prevYaw + 10) // Adjust the value for speed
+    }
+
+    const zoomIn = () => {
+        setHfov((prevHfov) => prevHfov - 10) // Adjust the value for speed
+    }
+
+    const zoomOut = () => {
+        setHfov((prevHfov) => prevHfov + 10) // Adjust the value for speed
+    }
+
+    return (
+        <>
+            <div>
+                pitch: {pitch}, yaw: {yaw}
+            </div>
+
+            <Pannellum
+                ref={panImage}
+                width="100%"
+                height="500px"
+                keyboardZoom={false}
+                // mouseZoom={false}
+                image={
+                    scenesArray[currentScene].scenePanoImg + '?resize=800%2C600'
+                }
+                pitch={scenesArray[currentScene].initPitch + pitch}
+                yaw={scenesArray[currentScene].initYaw + yaw}
+                hfov={hfov}
+                haov={360}
+                vaov={180}
+                autoLoad
+                showZoomCtrl={false}>
+                {scenesArray[currentScene].hotSpotsArr.map((hotSpot) => {
+                    return (
+                        <Pannellum.Hotspot
+                            key={hotSpot}
+                            type={hotSpot.type}
+                            pitch={hotSpot.pitch}
+                            yaw={hotSpot.yaw}
+                            tooltip={hotSpotIcon(hotSpot.type)}
+                            handleClick={() =>
+                                setCurrentScene(hotSpot.transition)
+                            }
+                            text={hotSpot.text}
+                            URL={hotSpot.url}
+                        />
+                    )
+                })}
+            </Pannellum>
+
+            <div className="flex flex-col">
+                <button onClick={moveForward}>Up</button>
+                <button onClick={moveBackward}>Down</button>
+                <button onClick={moveLeft}>Left</button>
+                <button onClick={moveRight}>Right</button>
+                <button onClick={zoomIn}>Zoom In</button>
+                <button onClick={zoomOut}>Zoom Out</button>
+            </div>
+        </>
+    )
+}
+
+export default Tour
